aa5679beb69f36e598d527cb74df019db8e7738c
Sunhokim2 <mcwright@naver.com>
2025-05-15 14:53:33 +0900

    최초추가

---
+++ backProject/src/test/java/org/example/backproject/BackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
