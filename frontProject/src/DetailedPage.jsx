d4df897eba5a09978934b232c1adf93e73c12400
Tndals61 <sungmin.jeon.rafael@gmail.com>
2025-05-21 09:55:10 +0900

    updated key name to match with the BE data

--- frontProject/src/DetailedPage.jsx
+++ frontProject/src/DetailedPage.jsx
@@ -4,41 +4,15 @@ import regionColors from './korea_regions_colors.json';
 import categoryColors from './category_colors.json'
 
 const DetailedPage = () => {
-  //   const placeData = {
-  //     id: 1,
-  //     imageUrl:
-  //       "https://i.namu.wiki/i/KJGxuoNhAdBUfU9wkTiW1dCGslf_KxfMF8SjlcfIl2ZeAaHXEYWT69f8D5QAXj12yxPibt9yBT3Y7hxiukU-qFI-PpJHUsKnqLzUMQMfExy6w4zhBoGpzjMqvob0cNcC7O-PA3XuHBZfwYgk4hI6PGquYgNyBxiau8wGMKrKToo.webp",
-  //     restaurant: "홍콩반점",
-  //     category: "음식점",
-  //     region: "강남구",
-  //     main_menu: "짜장면",
-  //     address: "서울 강남구 테헤란로 123",
-  //     body: "맛과 양 모두 만족스러운 중식당입니다.",
-  //     latitude: 37.500123,
-  //     longitude: 127.035678,
-  //     rate: 4,
-  //     source: "https://blog.naver.com/jieblog/223864819923",
-  //     status: "방문함",
-  //     bookmarked: true,
-  //     visited: true,
-  //     created_at: "2025-05-15",
-  //     user_posts: [
-  //       {
-  //         user_name: "전성민",
-  //         created_at: "2025-05-15T10:00:00Z",
-  //         latitude: 37.500124,
-  //         longitude: 127.035679,
-  //       },
-  //     ],
-  //   };
+  
   const placeData = {
     id: 3,
     imageUrl:
       "https://cdn.imweb.me/upload/S2024013025a8a2a1c6644/13570ac6b056e.png",
-    restaurant: "능동미나리 여의도점",
+    restaurant_name: "능동미나리 여의도점",
     category: "음식점",
     region: "영등포구",
-    main_menu: "미나리 수육 전골, 능동 육회비빔밥, 육전",
+    mainMenu: ["미나리 수육 전골", "능동 육회비빔밥", "육전"],
     address: "서울 영등포구 의사당대로 127 롯데캐슬 엠파이어빌딩 1층 107호",
     body: "미나리가 듬뿍 들어간 수육전골과 육회비빔밥, 육전이 유명한 맛집. 술과 함께 즐기기 좋은 구성으로 여의도 직장인들의 저녁 술자리 성지로 떠오르는 중. 깔끔한 매장과 아기의자 구비, 테이블링 현장대기 시스템 운영. 국물이 점점 깊어지는 전골과 간장양념 육회비빔밥, 육전+비빔밥 조합이 환상적.",
     latitude: 37.527265, // 여의도 롯데캐슬 엠파이어 좌표 (대략적)
@@ -100,7 +74,7 @@ const DetailedPage = () => {
           </button>
         </div>
         <div className="d-flex justify-content-between align-items-start mb-5">
-          <h1 className="mb-0">{placeData.restaurant}</h1>
+          <h1 className="mb-0">{placeData.restaurant_name}</h1>
           <div className="d-flex gap-2">
             <button
               className={`bookmark-button ${
@@ -121,13 +95,6 @@ const DetailedPage = () => {
         <div className="d-flex mb-2">
           <div className="detail-label">카테고리: </div>
           <div className="d-flex justify-content-between">
-            {/* <span
-              className={`badge ${
-                placeData.category.trim() === "음식점"
-                  ? "badge-category-restaurant"
-                  : "badge-category-cafe"
-              } me-2`}
-            > */}
             <span
                 className="badge"
                 style={{
@@ -157,7 +124,7 @@ const DetailedPage = () => {
         </div>
         <div className="d-flex mb-2">
           <div className="detail-label">대표 메뉴: </div>
-          <div>{placeData.main_menu}</div>
+          {placeData.main_menu.join(", ")}
         </div>
         <div className="d-flex mb-2">
           <div className="detail-label">주소: </div>
--- frontProject/src/MyPage.jsx
+++ frontProject/src/MyPage.jsx
@@ -5,166 +5,142 @@ import categoryColors from "./category_colors.json";
 
 const MyPage = () => {
   //dummy data
-  const placesData = [
+  const placesData =[
     {
-      imageUrl:
-        "https://image.tabling.co.kr/production/menu/fcc8ef2e-4a50-4fed-802c-c9d61e0a2fe8.jpeg",
-      restaurant: "능동미나리 여의도점",
-      category: "음식점",
+      imageUrl: "https://image.tabling.co.kr/production/menu/fcc8ef2e-4a50-4fed-802c-c9d61e0a2fe8.jpeg",
+      restaurant_name: "능동미나리 여의도점",
+      category: "한식",
       region: "영등포구",
-      main_menu: "미나리 수육 전골, 능동 육회비빔밥, 육전",
-      description:
-        "미나리 수육 전골과 육회비빔밥, 육전이 인기인 여의도 직장인들의 술자리 성지.",
-      bookmarked: true,
+      mainMenu: ["미나리 수육 전골", "능동 육회비빔밥", "육전"],
+      description: "미나리 수육 전골과 육회비빔밥, 육전이 인기인 여의도 직장인들의 술자리 성지.",
+      bookmarked: true
     },
     {
-      imageUrl:
-        "https://d12zq4w4guyljn.cloudfront.net/750_750_20250106052220_photo1_22dd42f297ce.webp",
-      restaurant: "소바하우스 멘야준 연남점",
-      category: "음식점",
+      imageUrl: "https://d12zq4w4guyljn.cloudfront.net/750_750_20250106052220_photo1_22dd42f297ce.webp",
+      restaurant_name: "소바하우스 멘야준 연남점",
+      category: "일식",
       region: "마포구",
-      main_menu: "특선 소유라멘, 완탕, 흑돼지 슈마이",
+      mainMenu: ["특선 소유라멘", "완탕", "흑돼지 슈마이"],
       description: "토핑의 퀄리티가 높은 라멘 맛집. 완탕과 슈마이가 특히 인기.",
-      bookmarked: false,
+      bookmarked: false
     },
     {
       imageUrl: "https://img.siksinhot.com/place/1640215450796032.jpeg",
-      restaurant: "도넛정수 창신",
+      restaurant_name: "도넛정수 창신",
       category: "카페",
       region: "종로구",
-      main_menu: "초코 바나나 도넛, 쑥 초코 도넛, 아메리카노, 바닐라라떼",
+      mainMenu: ["초코 바나나 도넛", "쑥 초코 도넛", "아메리카노"],
       description: "서울 절벽골목에서 서울 뷰와 함께 즐기는 수제 도넛 카페.",
-      bookmarked: true,
+      bookmarked: true
     },
     {
-      imageUrl:
-        "https://blog.kakaocdn.net/dn/bfdTpz/btrDFKZBOKJ/QSI1BH8CfvFOjeuFqf7VQk/img.jpg",
-      restaurant: "핫쵸 성수",
-      category: "음식점",
+      imageUrl: "https://blog.kakaocdn.net/dn/bfdTpz/btrDFKZBOKJ/QSI1BH8CfvFOjeuFqf7VQk/img.jpg",
+      restaurant_name: "핫쵸 성수",
+      category: "일식",
       region: "성동구",
-      main_menu: "히로시마 오코노미야끼, 명란 크림 돈페야끼, 야끼소바",
+      mainMenu: ["히로시마 오코노미야끼", "명란 크림 돈페야끼", "야끼소바"],
       description: "퍼포먼스 철판 요리가 볼거리인 성수 오코노미야끼 전문점.",
-      bookmarked: true,
+      bookmarked: true
     },
     {
-      imageUrl:
-        "https://d12zq4w4guyljn.cloudfront.net/750_750_20240408075829_photo1_b0799598ca1c.webp",
-      restaurant: "와일드플라워 방배",
-      category: "음식점",
+      imageUrl: "https://d12zq4w4guyljn.cloudfront.net/750_750_20240408075829_photo1_b0799598ca1c.webp",
+      restaurant_name: "와일드플라워 방배",
+      category: "양식",
       region: "서초구",
-      main_menu:
-        "트러플 들깨 궁채 파스타, 숨비소리 리조또, 레터링 서비스 디저트",
-      description:
-        "감성 가득한 방배동 레스토랑. 레터링 디저트 서비스로 기념일 추천.",
-      bookmarked: true,
+      mainMenu: ["트러플 들깨 궁채 파스타", "숨비소리 리조또", "레터링 서비스 디저트"],
+      description: "감성 가득한 방배동 레스토랑. 레터링 디저트 서비스로 기념일 추천.",
+      bookmarked: true
     },
     {
-      imageUrl:
-        "https://mblogthumb-phinf.pstatic.net/MjAyMzEwMTZfMTUw/MDAxNjk3NDIzMjM4MzQ3.8Noyj6gr_uhPryMvUJldWbYuT3TP0RfgQ3Ljvd9VJ88g.cM0se-jk5J8mDcBOGogmtpNVXUZ2QQOGf_zCm5uads4g.JPEG.flak4121/IMG_9041.jpg?type=w800",
-      restaurant: "카츠바이콘반 도산공원점",
-      category: "음식점",
+      imageUrl: "https://mblogthumb-phinf.pstatic.net/MjAyMzEwMTZfMTUw/MDAxNjk3NDIzMjM4MzQ3.8Noyj6gr_uhPryMvUJldWbYuT3TP0RfgQ3Ljvd9VJ88g.cM0se-jk5J8mDcBOGogmtpNVXUZ2QQOGf_zCm5uads4g.JPEG.flak4121/IMG_9041.jpg?type=w800",
+      restaurant_name: "카츠바이콘반 도산공원점",
+      category: "일식",
       region: "강남구",
-      main_menu: "상로스카츠, 미니 카레, 돈지루",
-      description:
-        "두툼한 특등심 돈카츠와 향신료 진한 카레가 인기인 도산공원 돈카츠 맛집.",
-      bookmarked: true,
+      mainMenu: ["상로스카츠", "미니 카레", "돈지루"],
+      description: "두툼한 특등심 돈카츠와 향신료 진한 카레가 인기인 도산공원 돈카츠 맛집.",
+      bookmarked: true
     },
     {
-      imageUrl:
-        "https://blog.kakaocdn.net/dn/B0jO4/btrSI9RAQ3k/Xwz65YNvR5zwOYbpNXx4p1/img.jpg",
-      restaurant: "그라데이션커피 성수",
+      imageUrl: "https://blog.kakaocdn.net/dn/B0jO4/btrSI9RAQ3k/Xwz65YNvR5zwOYbpNXx4p1/img.jpg",
+      restaurant_name: "그라데이션커피 성수",
       category: "카페",
       region: "성동구",
-      main_menu: "핸드드립 위스키 배럴, 코스타리카 하시엔다 코페이",
+      mainMenu: ["핸드드립 위스키 배럴", "코스타리카 하시엔다 코페이"],
       description: "위스키 배럴 숙성 원두가 시그니처인 핸드드립 전문 카페.",
-      bookmarked: true,
+      bookmarked: true
     },
     {
-      imageUrl:
-        "https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/3fy/image/OUgALVYv5pBILsEFofbmJsxevOw.jpg",
-      restaurant: "코끼리베이글 인사동",
+      imageUrl: "https://img1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/3fy/image/OUgALVYv5pBILsEFofbmJsxevOw.jpg",
+      restaurant_name: "코끼리베이글 인사동",
       category: "카페",
       region: "종로구",
-      main_menu: "버터 솔트 베이글, 아메리카노",
-      description:
-        "화덕에 구운 쫀득 담백한 베이글이 인기인 인사동 베이글 맛집.",
-      bookmarked: false,
+      mainMenu: ["버터 솔트 베이글", "아메리카노"],
+      description: "화덕에 구운 쫀득 담백한 베이글이 인기인 인사동 베이글 맛집.",
+      bookmarked: false
     },
     {
-      imageUrl:
-        "https://mblogthumb-phinf.pstatic.net/MjAyMzA4MjlfMSAg/MDAxNjkzMjQ1NDY2Njkz.YsBR2X50Px7fiUmwMjstUXtvp5JU4_P5oFsjLior_gsg.oBFdNtq7P9V4D1OhJylZqQ7NenR8XG-SRZIeHvGYqDMg.JPEG.kimsy9591/IMG_8510.jpg?type=w800",
-      restaurant: "호수식당 문의",
-      category: "음식점",
+      imageUrl: "https://mblogthumb-phinf.pstatic.net/MjAyMzA4MjlfMSAg/MDAxNjkzMjQ1NDY2Njkz.YsBR2X50Px7fiUmwMjstUXtvp5JU4_P5oFsjLior_gsg.oBFdNtq7P9V4D1OhJylZqQ7NenR8XG-SRZIeHvGYqDMg.JPEG.kimsy9591/IMG_8510.jpg?type=w800",
+      restaurant_name: "호수식당 문의",
+      category: "한식",
       region: "청주시",
-      main_menu: "청국장, 순두부찌개, 비지찌개",
+      mainMenu: ["청국장", "순두부찌개", "비지찌개"],
       description: "청국장과 돌솥밥, 누룽지로 유명한 청주 문의 맛집.",
-      bookmarked: false,
+      bookmarked: false
     },
     {
-      imageUrl:
-        "https://img.siksinhot.com/place/1443970776790487.jpg?w=560&h=448&c=Y",
-      restaurant: "안성댁부대찌개 대전",
-      category: "음식점",
+      imageUrl: "https://img.siksinhot.com/place/1443970776790487.jpg?w=560&h=448&c=Y",
+      restaurant_name: "안성댁부대찌개 대전",
+      category: "한식",
       region: "유성구",
-      main_menu: "부대찌개, 삼겹살",
+      mainMenu: ["부대찌개", "삼겹살"],
       description: "깔끔한 국물이 매력적인 대전 현지인 부대찌개 맛집.",
-      bookmarked: false,
+      bookmarked: false
     },
     {
-      imageUrl:
-        "https://mblogthumb-phinf.pstatic.net/MjAyMzA0MjZfMTU5/MDAxNjgyNDM5MjU1MzQ1.R0DPhaxUT2BI9CQRNummin7J8cb5uQVCc17Z9I_m8Nkg.Q_i1hxJRVXmdXBPXlyipsGr7HH3x1OJJZVB54IqOrIQg.JPEG.queen7165/IMG_8597.jpg?type=w800",
-      restaurant: "프루 Fru",
+      imageUrl: "https://mblogthumb-phinf.pstatic.net/MjAyMzA0MjZfMTU5/MDAxNjgyNDM5MjU1MzQ1.R0DPhaxUT2BI9CQRNummin7J8cb5uQVCc17Z9I_m8Nkg.Q_i1hxJRVXmdXBPXlyipsGr7HH3x1OJJZVB54IqOrIQg.JPEG.queen7165/IMG_8597.jpg?type=w800",
+      restaurant_name: "프루 Fru",
       category: "술집",
       region: "종로구",
-      main_menu: "모둠 사시미, 매콤 파스타, 참치 후토마키, 일본식 계란말이",
-      description:
-        "일본 감성의 다찌 이자카야. 사시미와 명란 계란말이가 인기이며 시티팝 분위기 물씬.",
-      bookmarked: false,
+      mainMenu: ["모둠 사시미", "매콤 파스타", "참치 후토마키"],
+      description: "일본 감성의 다찌 이자카야. 사시미와 명란 계란말이가 인기이며 시티팝 분위기 물씬.",
+      bookmarked: false
     },
     {
-      imageUrl:
-        "https://design.co.kr/wp-content/uploads/2024/07/%EC%B0%A8%EC%B0%A8%EC%9D%B4%ED%85%8C_%EB%B3%B8%EC%B0%A8-%ED%8C%8C%EB%A5%B4%ED%8E%98-832x1109.jpg",
-      restaurant: "차차이테",
+      imageUrl: "https://design.co.kr/wp-content/uploads/2024/07/%EC%B0%A8%EC%B0%A8%EC%9D%B4%ED%85%8C_%EB%B3%B8%EC%B0%A8-%ED%8C%8C%EB%A5%B4%ED%8E%98-832x1109.jpg",
+      restaurant_name: "차차이테",
       category: "카페",
       region: "용산구",
-      main_menu: "맞이차, 본차+과자, 밀크티+스콘",
-      description:
-        "차와 과자를 코스로 즐기는 한남동 예약제 티룸. 분위기 좋은 디저트 공간.",
-      bookmarked: false,
+      mainMenu: ["맞이차", "본차+과자", "밀크티+스콘"],
+      description: "차와 과자를 코스로 즐기는 한남동 예약제 티룸. 분위기 좋은 디저트 공간.",
+      bookmarked: false
     },
     {
-      imageUrl:
-        "https://d12zq4w4guyljn.cloudfront.net/750_750_20250218121009125_photo_8de4c3974a4b.webp",
-      restaurant: "금제",
-      category: "음식점",
+      imageUrl: "https://d12zq4w4guyljn.cloudfront.net/750_750_20250218121009125_photo_8de4c3974a4b.webp",
+      restaurant_name: "금제",
+      category: "일식",
       region: "관악구",
-      main_menu: "특로스가츠, 히레가츠, 가츠산도",
-      description:
-        "저온 조리로 튀긴 프리미엄 로스가츠가 인기인 신림 돈카츠 맛집.",
-      bookmarked: false,
+      mainMenu: ["특로스가츠", "히레가츠", "가츠산도"],
+      description: "저온 조리로 튀긴 프리미엄 로스가츠가 인기인 신림 돈카츠 맛집.",
+      bookmarked: false
     },
     {
-      imageUrl:
-        "https://dw82ptradz9jo.cloudfront.net/space/0DA35076-8B4C-4394-A735-FB2609655848/story/602a3043bf00be3ceaf9b90a/25ec3b91-168f-4de9-b341-b6cc89b27330",
-      restaurant: "아우트로 커피",
+      imageUrl: "https://dw82ptradz9jo.cloudfront.net/space/0DA35076-8B4C-4394-A735-FB2609655848/story/602a3043bf00be3ceaf9b90a/25ec3b91-168f-4de9-b341-b6cc89b27330",
+      restaurant_name: "아우트로 커피",
       category: "카페",
       region: "청주시",
-      main_menu: "아메리카노, 레몬소르베 음료, 베이커리",
-      description:
-        "청주 외곽 불멍 가능한 대형 카페. 감성 좌석과 사진 맛집으로 인기.",
-      bookmarked: false,
+      mainMenu: ["아메리카노", "레몬소르베 음료", "베이커리"],
+      description: "청주 외곽 불멍 가능한 대형 카페. 감성 좌석과 사진 맛집으로 인기.",
+      bookmarked: false
     },
     {
-      imageUrl:
-        "https://blog.kakaocdn.net/dn/HPbck/btsHVEtlXj4/xO7t7Gkha8PzUdkJZLr2xk/img.jpg",
-      restaurant: "새말해장국 본점",
-      category: "음식점",
+      imageUrl: "https://blog.kakaocdn.net/dn/HPbck/btsHVEtlXj4/xO7t7Gkha8PzUdkJZLr2xk/img.jpg",
+      restaurant_name: "새말해장국 본점",
+      category: "한식",
       region: "오산시",
-      main_menu: "우거지갈비탕, 얼큰내장탕, 해장국",
-      description:
-        "오산 3대 해장국 맛집. 돌솥밥과 함께 먹는 진한 국물 해장국으로 인기.",
-      bookmarked: false,
-    },
+      mainMenu: ["우거지갈비탕", "얼큰내장탕", "해장국"],
+      description: "오산 3대 해장국 맛집. 돌솥밥과 함께 먹는 진한 국물 해장국으로 인기.",
+      bookmarked: false
+    }
   ];
 
   const [placesData1, setPlacesData] = useState([]); // BE 에서 데이터 받아오기
@@ -266,7 +242,7 @@ const MyPage = () => {
               <img
                 src={place.imageUrl}
                 className="card-img-top"
-                alt={place.restaurant}
+                alt={place.restaurant_name}
               />
               {place.bookmarked && (
                 <span
@@ -278,7 +254,7 @@ const MyPage = () => {
               )}
             </div>
             <div className="card-body d-flex flex-column gap-2">
-              <div className="fw-bold fs-5">{place.restaurant}</div>
+              <div className="fw-bold fs-5">{place.restaurant_name}</div>
               <span
                 className="badge"
                 style={{
@@ -301,7 +277,7 @@ const MyPage = () => {
               >
                 {place.region}
               </span>
-              <div className="small text">대표 메뉴: {place.main_menu}</div>
+              <div className="small text">대표 메뉴: {place.mainMenu.join(", ")}</div>
               <div className="small text-muted">내용: {place.description}</div>
             </div>
           </div>
